2da2f6da0d1901e5cec54a63d59620b2dd9980c7
Anne-Berit Lemika <anne-berit@lemika.net>
2023-05-28 11:26:19 +0200

    My profile

--- src/components/pages/Me.jsx
+++ src/components/pages/Me.jsx
@@ -1,24 +1,53 @@
-import { Container } from "react-bootstrap";
-import Header from "../common/Header";
 import { useContext, useEffect } from "react";
 import AuthContext from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import Loading from "../common/Loading";
+import DisplayMessage from "../common/DisplayMessage";
+import { profileAPI } from "../../constants/api";
+import MyProfileItem from "../posts/Profile";
 
 function Me() {
   const [auth] = useContext(AuthContext);
-  const navigate = useNavigate();
+  const [profile, setMyProfile] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    if (!auth) {
-      navigate("/login");
+  useEffect(function () {
+    const URL = profileAPI + auth.name;
+    async function fetchData() {
+      try {
+        const response = await axios.get(URL, {
+          headers: {
+            Authorization: `Bearer ${auth.accessToken}`,
+          },
+        });
+
+        if (response.status === 200) {
+          setMyProfile(response.data);
+        } else {
+          setError("Faen!");
+        }
+      } catch (error) {
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
     }
-  });
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Loading animation="border" />;
+  }
+
+  if (error) {
+    return <DisplayMessage messageType="danger" message="Helvete" />;
+  }
 
   return (
     <>
-      <Container>
-        <Header title="My profile" />
-      </Container>
+      <MyProfileItem profile={profile} />
     </>
   );
 }
