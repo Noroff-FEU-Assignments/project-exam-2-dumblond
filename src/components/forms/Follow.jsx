4a7c19988c5fd87a833c0f2672a80586addfeed1
Anne-Berit Lemika <anne-berit@lemika.net>
2023-06-02 10:13:13 +0200

    Follow

--- src/components/forms/Follow.jsx
+++ src/components/forms/Follow.jsx
@@ -1,29 +1,59 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../../context/AuthContext";
+import { profileAPI } from "../../constants/api";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
+import DisplayMessage from "../common/DisplayMessage";
 
 function Follow({ profile }) {
+  const { param } = useParams();
   const [auth] = useContext(AuthContext);
+  let URL = profileAPI + "/" + `${param}/follow`;
+  const [error, setError] = useState(null);
 
   const following = profile.followers.find((user) => {
     return user.name === auth.name;
   });
 
-  if (!following) {
-    return <div>Hei Verden</div>;
-  }
-
   if (following) {
-    return <div>hadet på badet!</div>;
+    URL = `${profileAPI}/${profile.name}/unfollow`;
+  } else {
+    URL = `${profileAPI}/${profile.name}/follow`;
   }
 
-  console.log(following);
-  console.log(profile);
-  return <div>Follow</div>;
+  async function toggleFollow() {
+    try {
+      await axios({
+        method: "put",
+        url: URL,
+        headers: {
+          Authorization: `Bearer ${auth.accessToken}`,
+        },
+        data: {},
+      });
+    } catch (error) {
+      setError(error.toString());
+    }
+  }
+
+  if (error) {
+    return <DisplayMessage messageType="danger" message={error} />;
+  }
+
+  return (
+    <Button variant="secondary" onClick={() => toggleFollow()}>
+      {following ? "Unfollow" : "+ Follow"}
+    </Button>
+  );
 }
 
 export default Follow;
 
 Follow.propTypes = {
-  profile: PropTypes.func,
+  profile: PropTypes.shape({
+    name: PropTypes.string,
+    followers: PropTypes.any,
+  }),
 };
